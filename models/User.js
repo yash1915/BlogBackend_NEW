723c856d7e2fed70977ababecaa13459e391f76d
yash1915 <157246927+yash1915@users.noreply.github.com>
2025-09-27 02:17:11 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,7 +7,6 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true, trim: true },
     email: { type: String, required: true, trim: true, unique: true },
     password: { type: String, required: true },
-    additionalDetails: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Profile" },
     image: { type: String, required: true },
     resetPasswordToken: { type: String },
     resetPasswordExpires: { type: Date }, 
