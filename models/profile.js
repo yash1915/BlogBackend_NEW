83889e1e84fa5316fbc6256b2338830b058615c5
yash1915 <157246927+yash1915@users.noreply.github.com>
2025-09-27 02:33:50 +0530

    Update profile.js

--- models/profile.js
+++ models/profile.js
@@ -5,5 +5,28 @@ const profileSchema = new mongoose.Schema({
     dateOfBirth: { type: String },
     about: { type: String, trim: true },
 });
+/ Update profile
+exports.updateProfile = async (req, res) => {
+    try {
+        const { about } = req.body;
+        const userId = req.user.id;
+
+        // Seedhe User model ko update karein
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { about },
+            { new: true } // Taaki updated user return ho
+        ).select("-password");
+
+        res.status(200).json({ 
+            success: true, 
+            message: "Profile updated successfully", 
+            user: updatedUser 
+        });
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Failed to update profile" });
+    }
+};
 
 module.exports = mongoose.model("Profile", profileSchema);
