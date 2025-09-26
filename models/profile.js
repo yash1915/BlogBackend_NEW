7f489839c0b2b5a48f3cc07ae9f5e42bf0232cb8
yash1915 <157246927+yash1915@users.noreply.github.com>
2025-09-27 02:36:49 +0530

    Update profile.js

--- models/profile.js
+++ models/profile.js
@@ -1,10 +1,1 @@
-const mongoose = require("mongoose");
 
-const profileSchema = new mongoose.Schema({
-    gender: { type: String },
-    dateOfBirth: { type: String },
-    about: { type: String, trim: true },
-});
-
-
-module.exports = mongoose.model("Profile", profileSchema);
